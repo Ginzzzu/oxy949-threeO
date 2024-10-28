d9504a54754a59be0784fa65e7d25a8bcdbcacdc
Konstantin Kustov <oxy949@gmail.com>
2024-10-28 11:28:16 +0300

    Logic

--- scripts/lib/ThreeO.js
+++ scripts/lib/ThreeO.js
@@ -3,79 +3,51 @@ export class ThreeO {
   static async init() {
   }
 
-  static async roll(player, withHope) {
-    // Функция выбора случайного сообщения из таблицы
-    const getRandomMessageFromTable = async (tableUUID) => {
-      const table =  game.tables.find(t => t.uuid === tableUUID);
-      if (table) {
-        const roll = await table.roll();
-        return roll.results[0].text; // Получаем текст первого результата
-      }
-      return "";
-    };
-
-    let dicepoolPlayer = game.candles.playerDiceCountTextUUID;
-    let dicepoolGM = game.candles.gmDiceCountTextUUID;
-
-    let playerSuccessesTable = game.settings.get('oxy949-ten-candles', 'playerSuccessesTableUUID');
-    let playerFailureTable = game.settings.get('oxy949-ten-candles', 'playerFailureTableUUID');
-    let gmSuccessesTable = game.settings.get('oxy949-ten-candles', 'gmSuccessesTableUUID');
-    let gmFailureTable = game.settings.get('oxy949-ten-candles', 'gmFailureTableUUID');
-
-    let diceCountPlayers = parseInt(canvas.drawings.placeables[canvas.drawings.placeables.findIndex(drawing => drawing.id === dicepoolPlayer)].document.text);
-    let diceCountGM = parseInt(canvas.drawings.placeables[canvas.drawings.placeables.findIndex(drawing => drawing.id === dicepoolGM)].document.text);
-
+  static async roll(diceCount, modifier = 'normal') {
     let successes = 0;
+    let empty = 0;
     let failures = 0;
-    let rollMessage = 'Проверка повествования...';
 
-    let diceCount = player ? diceCountPlayers : diceCountGM;
-    let roll = await new Roll(`${diceCount}d6`).evaluate({ async: true });
-
-    if (player && withHope) {
-        console.log('Rolling Hope');
-        let rollHope = await new Roll('1df').evaluate({ async: true });
-        if (rollHope.terms[0].results[0].result >= 1) {
-            successes += 1;
-        }
-
-        let speaker = ChatMessage.getSpeaker({ actor: game.user.character });
-        await rollHope.toMessage({ rollMode: 'publicroll', flavor: 'Кость Надежды:', speaker });
-    }
+    let roll = await new Roll(`${diceCount}df`).evaluate({ async: true });
 
     roll.terms[0].results.forEach(r => {
-        if (r.result === 1) { failures += 1 }
-        if (r.result === 6) { successes += 1 }
+        if (r.result === -1) { failures += 1 }
+        if (r.result === 0) { empty += 1 }
+        if (r.result === 1) { successes += 1 }
     });
 
-    if (successes > 0) {
-        rollMessage = player ? await getRandomMessageFromTable(playerSuccessesTable) : await getRandomMessageFromTable(gmSuccessesTable);
-    } else {
-        rollMessage = player ? await getRandomMessageFromTable(playerFailureTable) : await getRandomMessageFromTable(gmFailureTable);
+    let rollMessage = "<p>";
+    rollMessage += `<strong style="font-size: large;">Бросок: (${modifier})</strong><br>`;
+    rollMessage += `<strong style="font-size: large;">-1: (${failures})</strong><br>`;
+    rollMessage += `<strong style="font-size: large;">0: (${empty})</strong><br>`;
+    rollMessage += `<strong style="font-size: large;">1: (${successes})</strong><br>`;
+    rollMessage += "</p>";
+
+    let resourceRemoved = 0;
+    let totalResult = 0;
+    if (modifier === 'normal'){
+      resourceRemoved = failures;
+      totalResult = successes - failures;
+    }
+    else if (modifier === 'hard'){
+      resourceRemoved = failures + empty;
+      totalResult = successes - failures - empty;
+    }
+    else if (modifier === 'easy'){
+      resourceRemoved = failures;
+      totalResult = successes - failures + empty;
     }
 
-    rollMessage += "<p>";
-    if (successes === 0) {
-        rollMessage += `<strong style="font-size: large;">ПРОВАЛ</strong>`;
-    } else {
-        if (successes === 1)
-            rollMessage += `<strong style="font-size: large;">УСПЕХ</strong>`;
-        else {
-            rollMessage += `<strong style="font-size: large;">УСПЕХ (${successes})</strong>`;
-        }
-    }
-
-    let statsMessage = ``;
-    if (player)
-        statsMessage += `<br>"1" выпало: <strong>${failures}</strong>`;
+    let statsMessage = `Итого нужно ресурса: ${resourceRemoved}`;
+    statsMessage += `Итого результат: ${totalResult}`;
 
     // Добавляем кнопку в сообщение
     let buttonId = `reduce-dice`;
-    if (player && failures > 0) {  // Уникальный флаг для каждого сообщения
+    if (true) {
       statsMessage += `<button id="${buttonId}" data-failures="${failures}" style="margin-top: 10px; margin-bottom: 10px;">Убрать все "1" (${failures} шт.) из пула игроков</button>`;
-  }
+    }
 
-    let flavor = `${rollMessage}${statsMessage}</p>`;
+    let flavor = `${rollMessage}${statsMessage}`;
 
     let speaker = ChatMessage.getSpeaker({ actor: game.user.character });
     let chatMessage = await roll.toMessage({ rollMode: 'publicroll', flavor, speaker });
